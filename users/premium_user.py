341e3db4f4e4afdbc7265e6863782b9608da9e38
ashish shukla <ashishhh1226@gmail.com>
2025-12-14 13:37:52 +0530

    Phase 2: premium user with cashback feature

--- users/premium_user.py
+++ users/premium_user.py
@@ -11,4 +11,4 @@ class PremiumUser(User):
         
         cashback_amount = amount * 0.05
         self.wallet._update_balance(cashback_amount)
-        print(f"Cashback added:{cashback_amount}")
+        print(f"Cashback added: {cashback_amount}")
