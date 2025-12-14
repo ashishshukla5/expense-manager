f9a38f27b36fb0d7420677a04b8b9f1c7f4f51e0
ashish shukla <ashishhh1226@gmail.com>
2025-12-14 13:22:38 +0530

    Phase 2: premium user with cashback feature

---
+++ users/premium_user.py
@@ -0,0 +1,14 @@
+from users.user import User
+
+class PremiumUser(User):
+    def user_type(self):
+        return "Premium"
+    
+    def cashback(self, amount):
+        if amount <= 0:
+            print("Invalid amount for cashback")
+            return
+        
+        cashback_amount = amount * 0.05
+        self.wallet._update_balance(cashback_amount)
+        print(f"Cashback added:{cashback_amount}")
